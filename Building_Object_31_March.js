670b48abc5f2ced942d26119c76c9c54c63873c8
Akash <34833061+machraakash@users.noreply.github.com>
2022-04-01 12:35:56 +0530

    Create Building_Object_31_March.js

---
+++ Building_Object_31_March.js
@@ -0,0 +1,18 @@
+/**
+ * Practice: Building objects
+ *
+ * - Create JavaScript objects based on objects in your current environment.
+ * - Give each object an identifiable name.
+ * - Create properties to describe the objects and set their values.
+ * - Find an object that has another object inside of it to create a nested object.
+ * - Test your objects in the browser console by accessing the entire object and its specific properties.
+ */
+const hourglass = {
+  hieght: 20,
+  width: 10,
+  weight: "100 Grams",
+  sand: {
+    color: "Green",
+    texture: "Granular",
+  },
+};
